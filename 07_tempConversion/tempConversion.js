95a51887f9940b873e3e85fbaf2bb6c06dc17f07
DMortal1 <cryptosodium@gmail.com>
2023-11-28 01:16:48 +0530

    Completed Ex 7
    Completed exercise '07_tempConversion' from The Odin Project 'JavaScript-Exercises'

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  return Math.round(10*(fTemp - 32)*5/9)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  return Math.round(10*(cTemp*9/5 + 32))/10;
 };
 
 // Do not edit below this line
