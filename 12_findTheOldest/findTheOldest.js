40131276005fcb2484b68f8e5f77868d1f0ca68f
DMortal1 <cryptosodium@gmail.com>
2023-12-01 01:23:30 +0530

    Completed Ex 12
    Completed exercise '12_findTheOldest' from The Odin Project 'JavaScript-Exercises'

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let getAge = function(birth, death) {
+        if(!death) {
+            death = new Date().getFullYear();
+        }
+        return death - birth;
+    }
+    let getElder = function(oldestPerson,currentPerson) {
+        let oldestAge = getAge(oldestPerson.yearOfBirth,oldestPerson.yearOfDeath);
+        let currentAge = getAge(currentPerson.yearOfBirth,currentPerson.yearOfDeath);
+        return (currentAge > oldestAge) ? currentPerson : oldestPerson;
+    }
+    return people.reduce(getElder);
 };
 
 // Do not edit below this line
