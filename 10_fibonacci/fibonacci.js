8b7a6db23abbfc29125a93201927ee03a4c5645d
DMortal1 <cryptosodium@gmail.com>
2023-12-01 00:15:03 +0530

    Completed Ex 10
    Completed exercise '10_fibonacci' from The Odin Project 'JavaScript-Exercises'

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(arg) {
+    if(arg < 3) {
+        if(arg > 0) {
+            return 1;
+        } else {
+            return "OOPS";
+        }
+    } else {
+        return fibonacci(arg - 1) + fibonacci(arg - 2);
+    }
 };
 
 // Do not edit below this line
